2a345e767905ed4e1a7a5a17d0faa802e07208e2
Slivinskas <slivinskasvytenis@gmail.com>
2016-06-03 21:59:16 +0300

    Kėlimas į github
    (cherry picked from commit fe69956d6e536f06855325c82033d010fdbf91d5)

--- app/src/main/java/com/example/slivinskas/muzikoszaidimas/FullscreenActivity.java
+++ app/src/main/java/com/example/slivinskas/muzikoszaidimas/FullscreenActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+//Mano naujas komentaras
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
